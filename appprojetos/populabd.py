9acb67c821aca69eb55be0f2cf915794895e5cdd
Juliana Soares <julianasoares195@hotmail.com>
2016-06-20 23:39:46 -0300

    Correção dos nomes das classes no populabd

--- appprojetos/populabd.py
+++ appprojetos/populabd.py
@@ -1,4 +1,5 @@
 from appprojetos.models import *
+
 # Criando projetos
 proj01=Projeto(titulo='Proj1', data_inicio='06/19/2016', data_termino='',
                justificativa='Projeto de pesquisa', metodologia='pesquisa', resultados_esperados='conclusão da pesquisa')
@@ -10,19 +11,19 @@ proj02.save()
 
 
 # Criando atividades
-ativ01 = AtividadesProjeto(descricao='Criar o cronograma',data_inicio='06/18/2016', data_termino='', custo='')
-ativ02 = AtividadesProjeto(descricao='Obter informações sobre o assunto',data_inicio='06/19/2016', data_termino='', custo='')
-ativ03 = AtividadesProjeto(descricao='Documentar o projeto',data_inicio='06/20/2016', data_termino='', custo='')
+ativ01 = AtividadeProjeto(descricao='Criar o cronograma',data_inicio='06/18/2016', data_termino='', custo='')
+ativ02 = AtividadeProjeto(descricao='Obter informações sobre o assunto',data_inicio='06/19/2016', data_termino='', custo='')
+ativ03 = AtividadeProjeto(descricao='Documentar o projeto',data_inicio='06/20/2016', data_termino='', custo='')
 ativ01.save()
 ativ02.save()
 ativ03.save()
 
 
 # Criando membros
-membro01 = Membros(nome='Juliana', cpf='1234567891011',data_nascimento='01/01/1995',
+membro01 = Membro(nome='Juliana', cpf='1234567891011',data_nascimento='01/01/1995',
                    email='juliana@email.com', telefone='9999-9990', matricula='20142148060154')
 
-membro02 = Membros(nome='Luan', cpf='1234567891012',data_nascimento='01/01/1993',
+membro02 = Membro(nome='Luan', cpf='1234567891012',data_nascimento='01/01/1993',
                    email='luan@email.com', telefone='8888-8880', matricula='20142148060120')
 membro01.save()
 membro02.save()
