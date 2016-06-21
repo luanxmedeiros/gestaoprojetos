d055242d1ff412d1c11cf7dc9f55b55aca7927b7
Luan Medeiros Macena <luanxmedeiros@live.com>
2016-06-20 22:43:51 -0300

    Corrigindo pull acidental...

--- appprojetos/models.py
+++ appprojetos/models.py
@@ -2,30 +2,45 @@ from django.db import models
 
 # Create your models here.
 
+
 class Projeto(models.Model):
     titulo = models.CharField("Título",max_length=256)
     data_inicio = models.DateField("Data de Início", null=False)
-    data_termino = models.DateField("Data de Término")
-    justificativa =  models.TextField("Justificativa", null=False)
-    metodologia = models.TextField("Metodologia", null=False)
-    resultados_esperados = models.TextField("Resultados Esperados", null=False)
+    data_termino = models.DateField("Data de Término", null=False)
+    justificativa =  models.TextField("Justificativa")
+    metodologia = models.TextField("Metodologia")
+    resultados_esperados = models.TextField("Resultados Esperados")
+    #Relacionamento muitos para muitos
+    membros = models.ManyToManyField(Membro, through="MembroProjeto")
+
 
 class AtividadesProjeto(models.Model):
-    descricao = models.CharField("Descrição", max_length=500, null=False)
+    descricao = models.CharField("Descrição", max_length=500)
     data_inicio = models.DateField("Data de Início", null=False)
-    data_termino = models.DateField("Data de Término")
+    data_termino = models.DateField("Data de Término", null=False)
     custo = models.DecimalField("Custo",max_digits=20, decimal_places=2)
     projeto = models.ForeignKey(Projeto,on_delete=models.PROTECT, verbose_name="Projeto")
 
-class Membros(models.Model):
-    nome = models.CharField("Nome", max_length=250, null=False)
-    cpf = models.CharField("CPF", max_length=11, unique=True, null=False)
+
+
+class Membro(models.Model):
+    nome = models.CharField("Nome", max_length=250)
+    cpf = models.CharField("CPF", max_length=11, unique=True)
     data_nascimento = models.DateField("Data de Nascimento", null=False)
     email = models.EmailField("E-Mail", max_length=200)
     telefone = models.CharField("Telefone", max_length=20)
-    matricula = models.CharField("Matrícula", max_length=14, unique=True, null=False)
+    matricula = models.CharField("Matrícula", max_length=14, unique=True)
+    #Relacionamento muitos para muitos
+    projetos = models.ManyToManyField(Projeto, through="MembroProjeto")
 
-class MembrosProjeto(models.Model):
+
+class MembroProjeto(models.Model):
     projeto = models.ForeignKey(Projeto, on_delete=models.PROTECT, verbose_name="Projeto")
-    membro = models.ForeignKey(Membros, on_delete=models.PROTECT, verbose_name="Membro")
+    membro = models.ForeignKey(Membro, on_delete=models.PROTECT, verbose_name="Membro")
     unique_together = ('projeto', 'membro') #Unicidade para que o membro não esteja no mesmo projeto duas vezes
+
+
+class DesenvolvimentoAtividade(models.Model):
+    estagio = models.CharField("Estágio", max_length=250, null=False)
+    em_desenvolvimento = models.CharField("Desenvolvendo", max_length=250, null=False)
+    atividade_projeto = models.ForeignKey(AtividadesProjeto, verbose_name="Atividade", null=False)
