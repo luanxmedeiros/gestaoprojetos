19f032d7fcf05f738ce58e53c67217110e349c55
Juliana Soares <julianasoares195@hotmail.com>
2016-06-21 21:09:03 -0300

    Consulta C pronta

--- appprojetos/consultas.py
+++ appprojetos/consultas.py
@@ -2,17 +2,21 @@
 # LETRA A - Listar todos os projetos com seus respectivos membros (Valor 0,2).
 from appprojetos.models import *
 for projetos in Projeto.objects.all():
-    print("Projeto:",projetos.titulo,"Membros:",membro.nome)
+    print("Projeto:",Projeto.titulo)
 '''
 # LETRA B - Listar todas as atividades que tenham sido executadas no mês de maio de 2015 (Valor 0,1).
-
-# LETRA C - Listar todos as pessoas que fazem parte do Staff da Universidade cujo o nome começa com a letra A (Valor 0,2)
 from appprojetos.models import *
-for membro in Membro.objects.filter(nome__startswith='L'):
+for atividade in AtividadeProjeto.objects.filter(atividade__data_inicio__month=5):
+    print("Atividade:",atividade)
+
+# LETRA C - Listar todos as pessoas que fazem parte do Staff da Universidade cujo o nome começa com a letra A
+from appprojetos.models import *
+for membro in Membro.objects.filter(nome__startswith='A'):
     print("Nome:",membro.nome)
 
 # LETRA D - Listar o custo total de cada projeto (Valor 0,5 - Extra).
 from appprojetos.models import *
 for custo in AtividadeProjeto.custo():
-    print("Custo:",AtividadeProjeto.custo)
+    print("Custo:",custo)
 '''
+
