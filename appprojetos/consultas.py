1594d1ac0279caff38a0a0a6134ad70e32b4e3dc
Juliana Soares <julianasoares195@hotmail.com>
2016-06-21 22:26:46 -0300

    Consulta B pronta

--- appprojetos/consultas.py
+++ appprojetos/consultas.py
@@ -1,13 +1,14 @@
-
+'''
 # LETRA A - Listar todos os projetos com seus respectivos membros (Valor 0,2).
 from appprojetos.models import *
 for projetos in Projeto.objects.all():
     print("Projeto:",Projeto.titulo)
-'''
+
 # LETRA B - Listar todas as atividades que tenham sido executadas no mês de maio de 2015 (Valor 0,1).
 from appprojetos.models import *
-for atividade in AtividadeProjeto.objects.filter(atividade__data_inicio__month=5):
-    print("Atividade:",atividade)
+for atividade in AtividadeProjeto.objects.filter(projeto__atividadeprojeto__data_inicio__year=2016):
+    for atividade in AtividadeProjeto.objects.filter(projeto__atividadeprojeto__data_inicio__month=6):
+        print("Atividade: ",atividade.descricao)
 
 # LETRA C - Listar todos as pessoas que fazem parte do Staff da Universidade cujo o nome começa com a letra A
 from appprojetos.models import *
@@ -19,4 +20,3 @@ from appprojetos.models import *
 for custo in AtividadeProjeto.custo():
     print("Custo:",custo)
 '''
-
